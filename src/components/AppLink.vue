4cde5e86765769f9611e6dbe72cb9c529de4e05a
emmanueluwa <emmanuel@fulodev.com>
2024-04-04 18:23:33 +0100

    adding router-link to header

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -1,1 +1,9 @@
-<template></template>
+<script setup lang="ts">
+const props = defineProps({ to: { type: String, required: true } });
+</script>
+
+<template>
+  <router-link :to="to" class="text-light-blue">
+    <slot></slot>
+  </router-link>
+</template>
